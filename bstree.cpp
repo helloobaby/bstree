460959403c5d7410aa8cec4190607cf5ca894146
123 <84119280+helloobaby@users.noreply.github.com>
2022-04-30 21:38:39 +0800

    Update bstree.cpp

--- bstree.cpp
+++ bstree.cpp
@@ -1,7 +1,16 @@
+/*
+2022.4.29(am) complete bs tree
+2022.4.29(pm) try to improve to avl tree
+2022.4.30   add single rotate and double rotate
+*/
+
 #include<iostream>
 #include<cassert>
-#include<tuple>
+#include<vector>
 #include<type_traits>
+#include<algorithm>
+#include<stack>
+#include<queue>
 using namespace std;
 
 template <typename T = int>
@@ -20,7 +29,8 @@ public:
         tree_node() {}
     };
     using node = tree_node;
-    using pnode = node*;
+    using pnode = node*;//vector<typename bstree<T>::pnode>
+    using cpnode = node const*;
 
     bstree() {}
     pnode get_root() { return root; }
@@ -29,20 +39,19 @@ public:
 
     struct find_struc
     {
-      pnode father;
-      pnode current;  
+        pnode father;
+        pnode current;
     };
     auto find(T val);
-
     void pre_order(pnode root);
     void in_order(pnode root);
     void post_order(pnode root);
-
     int child_counts(pnode node);
-
     bool delete_node(T val);
+    int get_node_depth(const cpnode node);
+    int get_node_height(const pnode node);
 private:
-    pnode root = nullptr; //vc会默认初始化为空指针,gcc不会
+    pnode root = nullptr; //vc会默认初始化为空指针,gcc不会,所以指定初始化为null比较稳妥
 
 };
 
@@ -53,8 +62,8 @@ template<typename T>
 bool bstree<T>::insert(T val)
 {
     //先查找一次,出现相同的插入失败
-    auto [a,b] = find(val);
-    if(a || b)
+    auto [a, b] = find(val);
+    if (a || b)
         return false;
 
     if (!root)
@@ -105,7 +114,7 @@ void bstree<T>::pre_order(pnode root) {
 }
 
 template<typename T>
-void bstree<T>::in_order(pnode root){
+void bstree<T>::in_order(pnode root) {
     if (!root)
         return;
     else {
@@ -116,10 +125,10 @@ void bstree<T>::in_order(pnode root){
 }
 
 template<typename T>
-void bstree<T>::post_order(pnode root){
-    if(!root)
+void bstree<T>::post_order(pnode root) {
+    if (!root)
         return;
-    else{
+    else {
         post_order(root->left);
         post_order(root->right);
         cout << root->val << " ";
@@ -127,33 +136,34 @@ void bstree<T>::post_order(pnode root){
 }
 
 template<typename T>
-auto bstree<T>::find(T val){
-    if(!root) 
-        return find_struc(0,0);
-    
+auto bstree<T>::find(T val) {
+    if (!root)
+        return find_struc(0, 0);
+
     auto t = root;
     pnode father = nullptr;
-    while(t){
-        if(val > t->val) //要查找的值大于这个节点,应该找右节点
+    while (t) {
+        if (val > t->val) //要查找的值大于这个节点,应该找右节点
         {
-            if(t->right){
+            if (t->right) {
                 father = t;
                 t = t->right;
             }
             else
-                return find_struc(0,0);
+                return find_struc(0, 0);
         }
-        else if(val < t->val){
-            if(t->left){
+        else if (val < t->val) {
+            if (t->left) {
                 father = t;
-                t=t->left;
-            }else
-                return find_struc(0,0);
+                t = t->left;
+            }
+            else
+                return find_struc(0, 0);
 
         }
         else //相等
-        {   
-            return find_struc(father,t);
+        {
+            return find_struc(father, t);
         }
     }
 
@@ -161,46 +171,46 @@ auto bstree<T>::find(T val){
 }
 
 //
-//给定一个树节点,返回这个树节点的树叶数量
+//给定一个树节点,返回这个树节点的树叶数量(仅左右子树),所以最大为2
 //
 template<typename T>
 int bstree<T>::child_counts(pnode node)
 {
-    if(node->left && node->right) //有左右节点
+    if (node->left && node->right) //有左右节点
         return 2;
-    else if( node->left && !node->right) //只有左节点
+    else if (node->left && !node->right) //只有左节点
         return 1;
-    else if( !node->left && node->right) //只有右节点
+    else if (!node->left && node->right) //只有右节点
         return 1;
     else //没有节点
         return 0;
 }
 
 template<typename T>
-bool bstree<T>::delete_node(T val){
-    auto [father,current] = find(val);
-    if(!father && !current)
+bool bstree<T>::delete_node(T val) {
+    auto [father, current] = find(val);
+    if (!father && !current)
         return false;
 
-    if(child_counts(current) == 0) //没有子节点,直接删除即可
+    if (child_counts(current) == 0) //没有子节点,直接删除即可
     {
-        if(father == nullptr)
+        if (father == nullptr)
         {
-            root = nullptr; 
+            root = nullptr;
             return true;
         }
 
-        if(father->left && father->left->val == current->val)
+        if (father->left && father->left->val == current->val)
             father->left = nullptr;
-        else if(father->right && father->right->val == current->val)
+        else if (father->right && father->right->val == current->val)
             father->right = nullptr;
         else
             assert(0); //find方法有误
     }
-    else if(child_counts(current) == 1){//要删除的节点只有一个子节点
+    else if (child_counts(current) == 1) {//要删除的节点只有一个子节点
 
-        if(father == 0){
-            if(father->left)
+        if (father == 0) {
+            if (father->left)
                 root = father->left;
             else
                 root = father->right;
@@ -208,50 +218,80 @@ bool bstree<T>::delete_node(T val){
             return true;
         }
 
-        if(father->left && father->left->val == current->val){ //要删除的节点是父节点的左节点
-            if(current->left) //
+        if (father->left && father->left->val == current->val) { //要删除的节点是父节点的左节点
+            if (current->left) //
                 father->left = current->left;
             else
                 father->left = current->right;
-        }else{ //删除的节点是父节点的右节点
+        }
+        else { //删除的节点是父节点的右节点
             assert(father->right);
 
-            if(current->left) //
+            if (current->left) //
                 father->right = current->left;
             else
                 father->right = current->right;
         }
-    } 
-    else if(child_counts(current) == 2){//要删除的节点有两个子节点
+    }
+    else if (child_counts(current) == 2) {//要删除的节点有两个子节点
         //将左子树挂到右子树的最左子树
 
-        if(father == nullptr)
+        if (father == nullptr)
             root = current->right;//根节点换成原右节点
-        else{
-        if(father->left && father->left->val == current->val)
-            father->left = current->right;
-        else
-            father->right = current->right;
+        else {
+            if (father->left && father->left->val == current->val)
+                father->left = current->right;
+            else
+                father->right = current->right;
         }
 
 
         auto recur_left_end = current->right;
-        while(true){
-            if(recur_left_end->left) //有左子树，不断地向下找最左子树
+        while (true) {
+            if (recur_left_end->left) //有左子树，不断地向下找最左子树
                 recur_left_end = recur_left_end->left;
             else
-                break; 
+                break;
         }
         recur_left_end->left = current->left;
 
 
-    } 
+    }
     else
         assert(0); //child_counts方法有误
 
     return true;
 }
 
+template<typename T>
+int bstree<T>::get_node_depth(const cpnode node)
+{
+    //因为每个节点只能有一个父节点,不断的向上找父节点,就可以求出节点的深度
+    int depth = 1;
+    auto father = find(node->val).father; //auto [father,current]
+    while (father) {
+        father = (find(father->val)).father;
+        depth++;
+    }
+
+
+    return depth;
+}
+
+template<typename T>
+int bstree<T>::get_node_height(const pnode node)
+{
+    if (!node)
+        return 0;
+
+    if (!node->left && !node->right)
+        return 1;
+
+    int h1 = get_node_height(node->left)+1;
+    int h2 = get_node_height(node->right)+1;
+
+    return std::max(h1, h2);
+}
 
 int main()
 {
@@ -263,12 +303,9 @@ int main()
     test.insert(20);
     test.insert(32);
     test.insert(45);
+    test.insert(68);
+    
 
-    test.insert(16);
-    test.delete_node(32);
-    test.delete_node(18);
-    test.delete_node(25);   
-    test.in_order(test.get_root()); cout << endl;
 
     return 0;
 }
